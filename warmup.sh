12bfbc64b9e35ab88f1ac14520c8ceb872869d33
Konstantin Podshumok <kpp.live@gmail.com>
2016-09-28 02:55:52 +0300

    call seafile's own inits

--- warmup.sh
+++ warmup.sh
@@ -2,9 +2,13 @@
 
 export ROOT=/usr/local/seafile/seafile-server
 export PYTHONPATH=$ROOT/:$ROOT/seahub:$ROOT/seahub/thirdpart:$PYTHONPATH
-export CCNET_CONF_DIR=/usr/local/seafile/conf/
+export CCNET_CONF_DIR=/usr/local/seafile/conf
+export INSTALLPATH=/usr/local/seafile/seafile-server/
+export SEAFILE_LD_LIBRARY_PATH=${INSTALLP${INSTALLPATH}/seafile/binATH}/seafile/lib/:${INSTALLPATH}/seafile/lib64:${LD_LIBRARY_PATH}
+export PATH=${INSTALLPATH}/seafile/bin:${PATH}
 
 mkdir -p /seafile-data/library-template || :
+mkdir -p ${CCNET_CONF_DIR}
 
 cd $ROOT
 python -m makedb
@@ -31,15 +35,37 @@ fi
 
 cat > /usr/local/seafile/conf/seafdav.conf <<EOF
 [WEBDAV]
-enabled = true
+enabled = ${WEBDAV_ENABLED:true}
 host= 0.0.0.0
 port = 8080
 fastcgi = ${WEBDAV_FASTCGI:-false}
 share_name = ${WEBDAV_SHARE_NAME:-/}
 EOF
 
-sed -i "s@SERVICE_URL = http://127.0.0.1:8000@SERVICE_URL = ${SERVICE_BASE:-$SITE_BASE/}@" \
-    ${CCNET_CONF_DIR}/ccnet.conf
+ccnet-init \
+    -c /usr/local/seafile/ccnet/ \
+    -F /usr/local/seafile/conf/ \
+    --name ${SITE_NAME} \
+    --host ${SITE_NAME} || echo 'using existing ccnet configuration'
+
+ccnet_id=$(grep ID /usr/local/seafile/conf/ccnet.conf | cut -f 3 -d ' ')
+ccnet_user=$(grep USER /usr/local/seafile/conf/ccnet.conf | cut -f 3 -d ' ')
+ccnet_url=$(grep SERVICE_URL /usr/local/seafile/conf/ccnet.conf | cut -f 3 -d ' ')
+
+cat > ${CCNET_CONF_DIR}/ccnet.conf <<EOF
+[General]
+USER_NAME = ${CCNET_USER:-${ccnet_user}}
+ID = ${CCNET_ID:-${ccnet_id}}
+NAME = ${CCNET_NAME:-${SITE_NAME}}
+SERVICE_URL = ${CCNET_URL:-${ccnet_url}}
+
+[Client]
+PORT = 13419
+EOF
+
+seaf_server_init \
+    --central-config-dir /usr/local/seafile/conf/ \
+    --seafile-dir /seafile-data/
 
 true | /usr/local/seafile/seafile-server/upgrade/minor-upgrade.sh
 
@@ -49,9 +75,8 @@ import os
 import check_init_admin
 
 if check_init_admin.need_create_admin():
-   check_init_admin.create_admin('${ADMIN_EMAIL:-admin@example.com}'),
+   check_init_admin.create_admin('${ADMIN_EMAIL:-admin@example.com}',
                                  '${ADMIN_PASSWORD:-youcannotguessit}')
-)
 EOF
 . /usr/local/bin/clean_env.sh
 
